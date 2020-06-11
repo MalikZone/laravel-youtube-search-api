58f5355e58583c1660cfc3c5e9a19033440143b7
MalikZone <ridwanmalik2010@gmail.com>
2020-06-11 14:09:30 +0700

    get data from youtube api

--- app/Http/Controllers/VideoSearchController.php
+++ app/Http/Controllers/VideoSearchController.php
@@ -13,11 +13,31 @@ class VideoSearchController extends Controller
      */
     public function index()
     {
-        $metaTittle = "Video Search";
+        // $metaTittle = "Video Search";
 
-        return view ('pages.videoSearch.videoSearch', [
-            'metaTittle' => $metaTittle,
-        ]);
+        // return view ('pages.videoSearch.videoSearch', [
+        //     'metaTittle' => $metaTittle,
+        // ]);
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://www.googleapis.com/youtube/v3/search?key=AIzaSyALIazv-Vj8z1XW4RnhNZvoaywAYKld6Dg&type=video",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        // echo $response;
+        dd(json_decode($response));
+        
     }
 
     /**
